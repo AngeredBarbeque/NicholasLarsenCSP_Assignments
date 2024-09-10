4446cc32b3f69501708fde22e015330e775d28c1
AngeredBarbeque <nick.larsen@ucas-edu.net>
2024-09-10 19:08:18 +0000

    Fixed it

--- firstProgram.c
+++ firstProgram.c
@@ -3,7 +3,7 @@
 int main(void) {
    char name[30];
    printf("Please tell me your name: \n");
-   fgets(name sizeof(name), stdin);
+   fgets(name, sizeof(name), stdin);
    printf("Hello %s", name);
    return 0;
 }
