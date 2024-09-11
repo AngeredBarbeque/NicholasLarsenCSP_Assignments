4dfe927b964c54f298c298af658e38f30d707e96
AngeredBarbeque <nick.larsen@ucas-edu.net>
2024-09-11 19:37:16 +0000

    Rearanged and completed expressions practice

---
+++ Expressions Practice/expressionsPractice.py
@@ -0,0 +1,19 @@
+#Nicholas Larsen Expressions practice
+
+print("equation one is", 7-24/8*4+6)
+
+print("equation two is", 18/3-7+2*5)
+
+print("equation three is", 6*4/12+72/8-9)
+
+print("equation four is", (17-6/2)+4*3)
+
+print("equation five is", -2*(1*4-2/2)+(6+2-3))
+
+print("equation six is", -1*((3-4*7)/5)-2*24/6)
+
+print("equation seven is", (3*5**2/15)-(5-2**2))
+
+print("equation eight is", (1**4*2**2+3**3)-2**5/4)
+
+print("equation nine is", (22/2-2*5)**2+(4-6/6)**2)
