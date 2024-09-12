48cfde3d5018bad3bd703de84ea0d2a2ea54acd5
AngeredBarbeque <nick.larsen@ucas-edu.net>
2024-09-12 19:39:35 +0000

    Added many things

--- Expressions Practice/budget.c
+++ Expressions Practice/budget.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
 int main(void){
-    double income[9]
+    char income[9];
+    char utilities[9];
+    char groceries[9];
+    char transportation[9];
     printf("This is a budget calculator.\n How much do you make a month?\n");
-    fgets(income, sizeof(income), stdin);
-    printf("You make %f", income);
+    scanf("%s", income);
+    printf("How much do you spend on utilities a month?\n");
+    scanf("%s", utilities);
+    printf("How much do you spend on groceries a month?\n");
+    scanf("%s", groceries);
+    printf("How much do you spend on transportation a month?\n");
+    scanf("%s", transportation);
+    printf("You make %s\n", income);
     return 0;
 }
