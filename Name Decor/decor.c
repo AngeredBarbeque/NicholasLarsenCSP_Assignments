5abada194b40cbeee8eee117b31e0b952f56a173
AngeredBarbeque <nick.larsen@ucas-edu.net>
2024-09-18 00:28:06 +0000

    Lataer

--- Name Decor/decor.c
+++ Name Decor/decor.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
-    char name[30];
-    printf("Tell me thine title by which thine ally's refer to thee as: ");
+    char name[70];
+    printf("What's your name?: ");
+    scanf("%s", name);
+    char decor[70] = "***";
+    strcat(name, decor);
+    strcat(decor, name);
+    printf("%s\n", decor);
     return 0;
 }
