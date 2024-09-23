6b374f9e75d7da2d5430bc52a4305acde17515f8
AngeredBarbeque <nick.larsen@ucas-edu.net>
2024-09-23 18:40:08 +0000

    commitee

---
+++ Updates/Hello.py
@@ -0,0 +1,9 @@
+
+
+def helloThere(name):
+    return("Hello", name)
+print(helloThere(name = "Jim"))
+print(helloThere(name = "Jim"))
+print(helloThere(name = "Jim"))
+print(helloThere(name = "Jim"))
+print(helloThere(name = "Jim"))
