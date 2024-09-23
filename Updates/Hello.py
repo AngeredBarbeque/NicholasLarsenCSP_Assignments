9159815dd539b7bfe661864c81c19855f85e1502
AngeredBarbeque <nick.larsen@ucas-edu.net>
2024-09-23 19:10:11 +0000

    Done

--- Updates/Hello.py
+++ Updates/Hello.py
@@ -1,9 +1,9 @@
 
 
 def helloThere(name):
-    return("Hello", name)
+    return("Hello " + name)
+print(helloThere(name = "John"))
 print(helloThere(name = "Jim"))
-print(helloThere(name = "Jim"))
-print(helloThere(name = "Jim"))
-print(helloThere(name = "Jim"))
-print(helloThere(name = "Jim"))
+print(helloThere(name = "Bob"))
+print(helloThere(name = "Tina"))
+print(helloThere(name = input("What's your name?: ")))
