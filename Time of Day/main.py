db02fee3ba9d472634c2ec3541a6d332b4d7bef8
AngeredBarbeque <nick.larsen@ucas-edu.net>
2024-09-30 19:24:42 +0000

    AHHH

---
+++ Time of Day/main.py
@@ -0,0 +1,12 @@
+import time
+
+inputTime = int(input("What is the time to the nearest hour? Use 24 hour clock: "))
+if inputTime <= 12 and inputTime >= 6:
+    print("Good morning!")
+elif inputTime >= 13 and inputTime <= 18:
+    print("Good afternoon!")
+elif inputTime >= 19 <= 24:
+    print("Good evening.")
+else:
+    print("Why are you up right now?")
+
